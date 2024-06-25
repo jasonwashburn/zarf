905e930170ba89dd56f494fe7e87f6b5f1860dba
Philip Laine <philip.laine@gmail.com>
2024-06-25 17:46:53 +0200

    refactor: remove use message.Fatal in cmd package

--- src/test/e2e/22_git_and_gitops_test.go
+++ src/test/e2e/22_git_and_gitops_test.go
@@ -13,7 +13,6 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/defenseunicorns/zarf/src/cmd/common"
 	"github.com/defenseunicorns/zarf/src/internal/packager/git"
 	"github.com/defenseunicorns/zarf/src/pkg/cluster"
 	"github.com/defenseunicorns/zarf/src/types"
@@ -69,8 +68,13 @@ func testGitServerConnect(t *testing.T, gitURL string) {
 }
 
 func testGitServerReadOnly(ctx context.Context, t *testing.T, gitURL string) {
+	timeoutCtx, cancel := context.WithTimeout(ctx, cluster.DefaultTimeout)
+	defer cancel()
+	c, err := cluster.NewClusterWithWait(timeoutCtx)
+	require.NoError(t, err)
+
 	// Init the state variable
-	state, err := common.NewClusterOrDie(ctx).LoadZarfState(ctx)
+	state, err := c.LoadZarfState(ctx)
 	require.NoError(t, err)
 
 	gitCfg := git.New(state.GitServer)
@@ -92,8 +96,13 @@ func testGitServerReadOnly(ctx context.Context, t *testing.T, gitURL string) {
 }
 
 func testGitServerTagAndHash(ctx context.Context, t *testing.T, gitURL string) {
+	timeoutCtx, cancel := context.WithTimeout(ctx, cluster.DefaultTimeout)
+	defer cancel()
+	c, err := cluster.NewClusterWithWait(timeoutCtx)
+	require.NoError(t, err)
+
 	// Init the state variable
-	state, err := common.NewClusterOrDie(ctx).LoadZarfState(ctx)
+	state, err := c.LoadZarfState(ctx)
 	require.NoError(t, err, "Failed to load Zarf state")
 	repoName := "zarf-public-test-2469062884"
 
